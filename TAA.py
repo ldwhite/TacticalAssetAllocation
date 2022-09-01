ca84e9a6263528957c86eceaece7c31dc8c56683
Lindsay Davies-White <81061866+ldwhite@users.noreply.github.com>
2022-09-01 08:42:13 -0400

    Update TAA.py

--- TAA.py
+++ TAA.py
@@ -6,21 +6,14 @@ import matplotlib.pyplot as plt
 from sklearn.metrics import mutual_info_score
 import math
 
-## Set up and load the data
-
 tickers = ['VTSMX', 'FDIVX', 'VEIEX', 'VFISX', 'VBMFX', 'DBC', 'VGSIX']
 
 start = dt(2019,1,4)
 end = dt(2022,8,30)
 
 data = web.DataReader(tickers, 'yahoo', start, end)['Adj Close']
-upro = web.DataReader('UPRO', 'yahoo', start, end)['Adj Close']
 
-tickers = data.columns.copy()
-tickers = tickers.to_list()
-data_srm = data[tickers]
-
-n = data_srm.shape[1]
+n = data.shape[1]
 ranks = [1, 2, 3]
 weights = np.array([1/ranks[-1]]*ranks[-1])
 lookback_period = 30
@@ -28,19 +21,11 @@ reallocation_period = 30
 
 capital = 1000
 
-# Create the first variable: Relative Strength, which Keller and van Putten (2012) refer to as Relative Momentum.
-
-## First, we create a DataFrame of the daily price differences
-
-delta = data_srm.diff()
-
-## Then, we create separate DataFrames for the days with positive change ('up') and the days with negative change ('down')
+delta = data.diff()
 
 up, down = delta.clip(lower=0), delta.clip(upper=0).abs()
 roll_up, roll_down = up.rolling(window = reallocation_period, center = False).mean(), down.rolling(window = reallocation_period, center = False).mean()
 r = (roll_up/roll_down)
-
-## Now, we create a placeholder DataFrame which we use to input the daily relative strength data
 
 rank_r1 = pd.DataFrame()
 
@@ -49,8 +34,6 @@ for i in range(len(r))[lookback_period::reallocation_period]:
     
 dates_r = rank_r1.columns.copy()
 r = r.iloc[lookback_period:]
-
-## Now, we create the final DataFrame, which ranks the top 3 securities in order of strength.
 
 rank_r = pd.DataFrame(index = ranks)
 
@@ -61,38 +44,27 @@ rank_r.columns = dates_r.copy()
 
 train = data.iloc[data.index.to_list().index(rank_r.T.index.to_list()[0]):]
 
-## Now, we create the portfolio DataFrame
-
 r_pfl = pd.DataFrame(index = train.index.to_list())
 r_pfl.index.name = 'Date'
 
 for i in ranks:
     r_pfl[i] = rank_r.T[i]
-
-## For ease of use, we construct a function called gen_pfl which generates the portfolio for us from the start time originally specified at the beginning to the end
     
 def gen_pfl(x, rank_x, hedge, weights, price, pfl):
     for i in ranks:
         pfl[i] = rank_x.T[i]
     
-    ## Filling in the first row.
-    
     pfl.ffill(inplace=True)
-    
-    ### The columns 'No x' refers to the number of shares we will be purchasing at the beginning of each month, in order for the portfolio to be appropriately balanced.
-    
+
     pfl['No 1'] = np.floor(weights[0]*capital/price[pfl[1].iloc[0]].iloc[0])
     pfl['No 2'] = np.floor(weights[0]*capital/price[pfl[2].iloc[0]].iloc[0])
     pfl['No 3'] = np.floor(weights[0]*capital/price[pfl[3].iloc[0]].iloc[0])
-    
-    ### The volumns 'Val x' are the actual dollar value of the respective security
-    
+
     pfl['Val 1'] = price[pfl[1].iloc[0]]*pfl['No 1'].iloc[0]
     pfl['Val 2'] = price[pfl[2].iloc[0]]*pfl['No 2'].iloc[0]
     pfl['Val 3'] = price[pfl[3].iloc[0]]*pfl['No 3'].iloc[0]
 
-    ## The column 'Asset Total' merely totals the dollar value of all of our investments
-    
+
     pfl['Asset Total'] = pfl['Val 1']\
                              + pfl['Val 2']\
                              + pfl['Val 3']\
@@ -101,9 +73,6 @@ def gen_pfl(x, rank_x, hedge, weights, price, pfl):
     pfl['Cash'] = capital - pfl['Asset Total'].iloc[0]
     pfl['Portfolio'] = pfl['Cash'] + pfl['Asset Total']
 
-    ## Now, we fill in the rest of our portfolio DataFrame, reblanacing our allocations every month and allocate to our hedging strategy whenever the metric 'x' (which will usually be Absolute Momentum) is negative.
-    ## NB: When you print the final DataFrame, the columns '1', '2', and '3' will still show the ranks of the securities, even if all their metrics' 'x' are negative but, rest-assured, the portfolio has been reallocated to our hedge
-    
     for i in range(len(pfl))[reallocation_period::reallocation_period]:
         
         if x[pfl[1].iloc[i]].iloc[i] < 0:
@@ -144,41 +113,37 @@ def gen_pfl(x, rank_x, hedge, weights, price, pfl):
     
         pfl['Cash'][i:] = pfl['Portfolio'].iloc[i-1] - pfl['Asset Total'][i]
         pfl['Portfolio'][i:] = pfl['Asset Total'][i:] + pfl['Cash'][i:]
-
-## Because we will not have our portfolio reallocate to our hedge when Relative Strength is negative, we construct a simple DataFrame filled with '1' as a placeholder.
         
 ones = pd.DataFrame(np.ones([len(r.index), len(r.columns)]), index = r.index, columns = r.columns)
 
-## Now, we generate our first DataFrame. Note that the variable we input for the 'hedge' variable is data['VFISX'] as, per Keller and van Putten (2012), we're using the Vanguard Short-Term Treasury Fund ETF as a cash proxy fund.
-
 gen_pfl(ones, rank_r, data['VFISX'], weights, train, r_pfl)
 
-## Now, we create our benchmark, which is a Buy & Hold strategy based on the seven funds we selected above.
-
-bh = pd.DataFrame(index = data_srm.iloc[lookback_period:].index)
+bh = pd.DataFrame(index = data.iloc[lookback_period:].index)
 cap_alloc = capital/len(tickers)
 
 for i in tickers:
-    bh[i] = data_srm[i].iloc[lookback_period:]*(np.floor(cap_alloc/data_srm[i].iloc[lookback_period]))
+    bh[i] = data[i].iloc[lookback_period:]*(np.floor(cap_alloc/data[i].iloc[lookback_period]))
 
 bh['Assets'] = bh.iloc[:, 0:len(tickers)].sum(axis=1)
 bh['Cash'] = capital - bh['Assets'].iloc[0]
 bh['Portfolio'] = bh['Cash'] + bh['Assets']
+
+spx = web.DataReader('^GSPC', 'yahoo', r_pfl.index.to_list()[0].to_pydatetime(), r_pfl.index.to_list()[-1].to_pydatetime())['Adj Close']
+spx = pd.DataFrame(spx*(np.floor(capital/spx.iloc[0])))
+spx['Cash'] = capital - spx['Adj Close'].iloc[0]
+spx['Portfolio'] = spx['Cash'] + spx['Adj Close']
 
 plt.figure(figsize = (12,8), dpi = 200)
 plt.plot((r_pfl['Portfolio']), label = 'Backtest')
 plt.plot((bh['Portfolio']), label = 'Benchmark')
 plt.legend()
 plt.title('Relative Momentum vs Buy & Hold')
-plt.savefig('r.png')
+plt.savefig('r.svg')
 plt.show()
 
-# Next Step is to construct a DataFrame for Absolute Momentum, we do this by merely taking the rate of change over the lookback period.
+m = (data.diff(lookback_period))/data.shift(lookback_period)
 
-m = (data_srm.diff(lookback_period))/data_srm.shift(lookback_period)
 m = m.iloc[lookback_period:]
-
-## To construct the DataFrame with the ranked values, we follow the same procedure as for Relative Strength, above.
 
 rank_m1 = pd.DataFrame()
 
@@ -200,8 +165,6 @@ rm_pfl.index.name = 'Date'
 
 for i in ranks:
     rm_pfl[i] = rank_rm.T[i]
-
-## We then generate the portfolio for Relative & Absolute Momenta and compare it to our benchmark
     
 gen_pfl(m, rank_rm, data['VFISX'], weights, train, rm_pfl)
 
@@ -210,18 +173,12 @@ plt.plot((rm_pfl['Portfolio']), label = 'Backtest')
 plt.plot((bh['Portfolio']), label = 'Benchmark')
 plt.legend()
 plt.title('Relative & Absolute Momentum vs Buy & Hold')
-plt.savefig('rm.png')
+plt.savefig('rm.svg')
 plt.show()
 
-# Next step is to utilize volatility in our model
+mv_weights1 = np.array([1/2, 1/2])
 
-## First, we construct an array called mv_weights which determines how much of an impact each characteristic has on a given security's rank.
-
-mv_weights = np.array([1/2, 1/2])
-
-## I don't like to use standard deviation as a measure of volatility, so I use a modification of GARCH
-
-r = np.log(data_srm).diff()
+r = np.log(data).diff()
 v = np.sqrt(np.square(r).rolling(lookback_period).sum())
 
 rank_v = pd.DataFrame()
@@ -231,7 +188,7 @@ for i in range(len(v))[lookback_period::reallocation_period]:
     
 dates_v = rank_v.columns
 
-mv = (mv_weights[0]*(rank_r1.T + rank_m1.T) + mv_weights[1]*(rank_v.T)).T.rank()
+mv = (mv_weights1[0]*(rank_r1.T + rank_m1.T) + mv_weights1[1]*(rank_v.T)).T.rank()
 
 dates_mv = mv.columns.copy()
 rank_mv = pd.DataFrame(index = ranks)
@@ -251,16 +208,10 @@ plt.plot((mv_pfl['Portfolio']), label = 'Backtest')
 plt.plot((bh['Portfolio']), label = 'Benchmark')
 plt.legend()
 plt.title('Momentum & Volatility vs Buy & Hold')
-plt.savefig('mv.png')
+plt.savefig('mv.svg')
 plt.show()
 
-## You can see now that not only has our model improved substantially but our drawdowns are significantly reduced and we would have come out of the 2020 crash relatively unscathed (compared to many other investors).
-
-## Finally, we generate a DataFrame containing the values for a rolling average correlation of each security with the other securities in our basket.
-
 c = pd.DataFrame()
-
-## The first step in doing so, is constructing a helper function that we will call avg_corr that, for the variable 'x', constructs a DataFrame with the rolling correlation of security 'x' with all other securities in the basket, then takes the average.
 
 def avg_corr(x):
     c1 = pd.DataFrame()
@@ -268,7 +219,7 @@ def avg_corr(x):
     mask.remove(mask[x])
     
     for i in range(len(mask)):
-        c1[i] = data_srm[tickers[x]].rolling(lookback_period).corr(data_srm[mask[i]])
+        c1[i] = data[tickers[x]].rolling(lookback_period).corr(data[mask[i]])
 
     c1.columns = mask
 
@@ -278,12 +229,10 @@ def avg_corr(x):
         c1['mean'].iloc[i] = c1.iloc[i].mean()
 
     c[x] = c1['mean']
-
-## Running the avg_corr() function for each ticker in the tickers list will add a column containing all those averages to the 'c' DataFrame. Thus, to have the DataFrame contain the list of average correlations for all the tickers in our basket, we use a for-loop to iterate over the list.
     
 for i in range(len(tickers)):
     avg_corr(i)
-
+    
 c.columns = tickers
 
 rank_c = pd.DataFrame()
@@ -293,8 +242,8 @@ for i in range(len(c))[lookback_period::reallocation_period]:
     
 dates_c = rank_c.columns.copy()
 
-mvc_weights = np.array([1, 1/2, 1/2])
-mvc = (mvc_weights[0]*(rank_r1.T + rank_m1.T) + mvc_weights[1]*(rank_v.T) + mvc_weights[2]*(rank_c.T)).T.rank()
+mvc_weights1 = np.array([1, 1/2, 1/2])
+mvc = (mvc_weights1[0]*(rank_r1.T + rank_m1.T) + mvc_weights1[1]*(rank_v.T) + mvc_weights1[2]*(rank_c.T)).T.rank()
 
 dates_mvc = mvc.columns.copy()
 rank_mvc = pd.DataFrame(index = ranks)
@@ -317,8 +266,6 @@ plt.legend()
 plt.savefig('mvc.png')
 plt.show()
 
-## Up to this point, we've been working with equal weightings for all of our securities but, surely, we would want to top-performing security to have the largest weighting in order for us to maximize our returns.
-
 u_weights = np.array([1/2, 1/3, 1/6])
 
 gen_pfl(m, rank_mvc, data['VFISX'], u_weights, train, mvc_pfl)
@@ -331,4 +278,69 @@ plt.legend()
 plt.savefig('mvc_ue.png')
 plt.show()
 
-print('Done!')
+def calc_MI(x, y):
+
+    n = len(x)
+    bins = math.floor(math.sqrt(n/5))
+    
+    c_xy = np.histogram2d(x, y, bins)[0]
+    mis = mutual_info_score(None, None, contingency=c_xy)
+    return mis
+
+mi = pd.DataFrame()
+
+def avg_mi(x):
+    mi1 = pd.DataFrame(index = data.index)
+    mask = tickers.copy()
+    mask.remove(mask[x])
+
+    for c in range(len(mask)):
+        mi1[c] = np.nan
+
+        for i in range(len(data))[30:]:
+            mi1[c].iloc[i] = calc_MI(data[tickers[x]].iloc[i-30:i], data[mask[c]].iloc[i-30:i])
+
+    mi1.columns = mask
+
+    mi1['Mean'] = np.NaN
+
+    for i in range(len(mi1)):
+        mi1['Mean'].iloc[i] = mi1.iloc[i].mean()
+
+    mi[x] = mi1['Mean']
+    
+for i in range(len(tickers)):
+    avg_mi(i)
+
+mi.columns = tickers
+
+rank_mi = pd.DataFrame()
+
+for i in range(len(mi))[lookback_period::reallocation_period]:
+    rank_mi = pd.concat([rank_mi, mi.iloc[i].rank(ascending = True)], axis = 1)
+    
+dates_mi = rank_mi.columns.copy()
+
+mvmi_weights = np.array([1, 1, 1])
+mvmi = (mvmi_weights[0]*(rank_r1.T + rank_m1.T) + mvmi_weights[1]*(rank_v.T) + mvmi_weights[2]*(rank_mi.T)).T.rank()
+
+dates_mvmi = mvmi.columns.copy()
+rank_mvmi = pd.DataFrame(index = ranks)
+
+for i in range(len(dates_mvmi)):
+    rank_mvmi[i] = mvmi[mvmi.columns[i]].sort_values().iloc[:ranks[-1]].index.to_list()
+
+rank_mvmi.columns = dates_mvmi
+
+mvmi_pfl = pd.DataFrame(index = train.index.to_list())
+mvmi_pfl.index.name = 'Date'
+
+gen_pfl(m, rank_mvmi, data['VFISX'], weights, train, mvmi_pfl)
+
+plt.figure(figsize = (12,8), dpi = 200)
+plt.plot(np.log(mvmi_pfl['Portfolio']), label = 'Backtest')
+plt.plot(np.log(bh['Portfolio']), label = 'Benchmark')
+plt.title('Tactical Asset Allocation with Momentum, Volatility, & Mutual Information')
+plt.legend()
+plt.savefig('mvmi.png')
+plt.show()
